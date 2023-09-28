40e85bd1f1ee2ece352f01ea712b66754a9a0dea
kingsley-sunny <ezeobisunny51@gmail.com>
2023-09-28 22:17:50 +0100

    feature: Added Documentation

---
+++ README.md
@@ -0,0 +1,27 @@
+## Kodecamp Blog api
+
+This is a simple blog api built with express js, prisma and mongodb ( for database ) and this are all the available routes:
+
+### 1. auth/register
+
+    To Register
+
+### 2. auth/login
+
+    To Login to your account
+
+### 3. auth/posts
+
+    To Get the list of post must be a GET request
+
+### 4. auth/posts/:postId
+
+    To get a single post by its id
+
+### 5. auth/posts/:postId - (PATCH)
+
+    To Edit a single post by its Id
+
+### 6s. auth/posts/:postId - (DELETE)
+
+    To Delete a post by its id
--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const authRoute = require("./routes/auth.routes");
 const postRoute = require("./routes/post.routes");
 const errorHandler = require("./middlewares/errorHandler");
+const CustomErrorApi = require("./helpers/customErrorApi");
+const { StatusCodes } = require("http-status-codes");
 require("express-async-errors");
 
 const app = express();
@@ -13,8 +15,8 @@ dotenv.config();
 app.use("/auth", authRoute);
 app.use("/posts", postRoute);
 
-app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+app.use((req, res, next) => {
+  return next(new CustomErrorApi(`Route Not Found: ${req.originalUrl}`, StatusCodes.NOT_FOUND));
 });
 
 app.use(errorHandler);
